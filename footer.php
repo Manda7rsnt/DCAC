70832ee9c94b9592391990577227764ef4bb9406
Vidit Tiwari <vidittiwari95@gmail.com>
2015-08-12 17:10:20 +0530

    header-footer

---
+++ footer.php
@@ -0,0 +1,182 @@
+
+        <div id="k-footer"><!-- footer -->
+
+            <div class="container"><!-- container -->
+
+                <div class="row no-gutter"><!-- row -->
+                    <div class="col-lg-4 col-md-4"><!-- widgets column center -->
+
+                        <div class="col-padded col-naked">
+
+                            <ul class="list-unstyled clear-margins"><!-- widgets -->
+
+                                <li class="widget-container widget_recent_news"><!-- widgets list -->
+
+                                    <h1 class="title-widget">College Address</h1>
+
+                                    <div itemscope itemtype="http://data-vocabulary.org/Organization"> 
+
+                                        <h2 class="title-median m-contact-subject" itemprop="name">Delhi College of Arts and Commerce</h2>
+
+                                        <div class="m-contact-address" itemprop="address" itemscope itemtype="http://data-vocabulary.org/Address">
+                                            <span class="m-contact-street" itemprop="street-address">Netaji Nagar</span>
+                                            <span class="m-contact-city-region"><span class="m-contact-city" itemprop="locality">New Delhi
+                                                </span>
+                                                <span class="m-contact-region" itemprop="region">Delhi</span></span>
+                                            <span class="m-contact-zip-country"><span class="m-contact-zip" itemprop="postal-code">110023</span> <
+                                                </div>
+
+                                                <div class="m-contact-tel-fax">
+                                                    <span class="m-contact-tel">Tel: <span itemprop="tel">011-24109821</span></span>
+
+                                                </div>
+
+                                                </div>
+
+                                            <div class="social-icons">
+
+                                                <ul class="list-unstyled list-inline">
+
+                                                    <li><a href="index.html#" title="Contact us"><i class="fa fa-envelope"></i></a></li>
+                                                    <li><a href="index.html#" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+                                                    <li><a href="index.html#" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+
+                                                </ul>
+
+                                            </div>
+
+                                            </li>
+
+                                        </ul>
+
+                                </div>
+
+                        </div><!-- widgets column center end -->
+
+                        <div class="col-lg-4 col-md-4"><!-- widgets column left -->
+
+                            <div class="col-padded col-naked">
+
+                                <ul class="list-unstyled clear-margins"><!-- widgets -->
+
+                                    <li class="widget-container widget_nav_menu"><!-- widgets list -->
+
+                                        <h1 class="title-widget">Useful links</h1>
+
+                                        <ul>
+                                            <li><a href="index.html#" title="menu item">Admissions</a></li>
+                                            <li><a href="index.html#" title="menu item">Academics</a></li>
+                                            <li><a href="index.html#" title="menu item">Faculties</a></li>
+                                            <li><a href="index.html#" title="menu item">Job Opportunities - Application</a></li>
+                                            <li><a href="index.html#" title="menu item">Farewell</a></li>
+                                        </ul>
+
+                                    </li>
+
+                                </ul>
+
+                            </div>
+
+                        </div><!-- widgets column left end -->
+
+
+
+                        <div class="col-lg-4 col-md-4"><!-- widgets column right -->
+
+                            <div class="col-padded col-naked">
+
+                                <ul class="list-unstyled clear-margins"><!-- widgets -->
+
+                                    <li class="widget-container widget_sofa_flickr"><!-- widgets list -->
+
+                                        <h1 class="title-widget">Gallery</h1>
+
+                                        <ul class="k-flickr-photos list-unstyled">
+                                            <li><a href="index.html#" title="Flickr photo"><img src="img/flickr-1.jpg" alt="Photo 1" /></a></li>
+                                            <li><a href="index.html#" title="Flickr photo"><img src="img/flickr-2.jpg" alt="Photo 2" /></a></li>
+                                            <li><a href="index.html#" title="Flickr photo"><img src="img/flickr-3.jpg" alt="Photo 3" /></a></li>
+                                            <li><a href="index.html#" title="Flickr photo"><img src="img/flickr-4.jpg" alt="Photo 4" /></a></li>
+                                            <li><a href="index.html#" title="Flickr photo"><img src="img/flickr-5.jpg" alt="Photo 5" /></a></li>
+                                            <li><a href="index.html#" title="Flickr photo"><img src="img/flickr-6.jpg" alt="Photo 6" /></a></li>
+                                            <li><a href="index.html#" title="Flickr photo"><img src="img/flickr-7.jpg" alt="Photo 7" /></a></li>
+                                            <li><a href="index.html#" title="Flickr photo"><img src="img/flickr-8.jpg" alt="Photo 8" /></a></li>
+                                        </ul>
+
+                                    </li>
+
+                                </ul> 
+
+                            </div>
+
+                        </div><!-- widgets column right end -->
+
+                    </div><!-- row end -->
+
+                </div><!-- container end -->
+
+            </div><!-- footer end -->
+
+            <div id="k-subfooter"><!-- subfooter -->
+
+                <div class="container"><!-- container -->
+
+                    <div class="row"><!-- row -->
+
+                        <div class="col-lg-12">
+
+                            <p class="copy-text text-inverse">
+                                &copy; 2015 Delhi College of Arts and Commerce. All rights reserved.<br>
+                                Developed & Maintained by <a href="">COFFEE COMRADES</a>
+                            </p>
+
+                        </div>
+
+                    </div><!-- row end -->
+
+                </div><!-- container end -->
+
+            </div><!-- subfooter end -->
+
+            <!-- jQuery -->
+            <script src="jQuery/jquery-2.1.1.min.js"></script>
+               
+                <!--Script for Ticker-->
+            <script type="text/javascript">
+function ticker() {
+    $('#ticker li:first').slideUp(function() {
+        $(this).appendTo($('#ticker')).slideDown();
+    });
+}
+setInterval(ticker, 3000);
+</script>
+                 <!--Script for Ticker-->
+                 
+                 
+            <script src="jQuery/jquery-migrate-1.2.1.min.js"></script>
+
+            <!-- Bootstrap -->
+            <script src="bootstrap/js/bootstrap.min.js"></script>
+
+            <!-- Drop-down -->
+            <script src="js/dropdown-menu/dropdown-menu.js"></script>
+
+            <!-- Fancybox -->
+            <script src="js/fancybox/jquery.fancybox.pack.js"></script>
+            <script src="js/fancybox/jquery.fancybox-media.js"></script><!-- Fancybox media -->
+
+            <!-- Responsive videos -->
+            <script src="js/jquery.fitvids.js"></script>
+
+            <!-- Audio player -->
+            <script src="js/audioplayer/audioplayer.min.js"></script>
+
+            <!-- Pie charts -->
+            <script src="js/jquery.easy-pie-chart.js"></script>
+
+
+
+            <!-- Theme -->
+            <script src="js/theme.js"></script>
+
+            </body>
+     
---
+++ sample-page.php
@@ -0,0 +1,11 @@
+<?php require('header.php');
+?>
+
+
+</div><!-- container end -->
+
+        </div><!-- content wrapper end -->
+
+<?php require('footer.php');
+
+?>
