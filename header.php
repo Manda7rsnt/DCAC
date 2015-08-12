d503270fe1892df8474e48e1e1c5af6e590fec5f
Vidit Tiwari <vidittiwari95@gmail.com>
2015-08-12 17:26:26 +0530

    sample-page

--- header.php
+++ header.php
@@ -94,30 +94,7 @@ header{
 
             <div class="container"><!-- container -->
 
-                <div class="row"><!-- row -->
-
-                    <div id="k-top-search" class="col-lg-12 clearfix"><!-- top search -->
-
-                        <form action="index.html#" id="top-searchform" method="get" role="search">
-                            <div class="input-group">
-                                <input type="text" name="s" id="sitesearch" class="form-control" autocomplete="off" placeholder="Type in keyword(s) then hit Enter on keyboard" />
-                            </div>
-                        </form>
-
-                        <div id="bt-toggle-search" class="search-icon text-center"><i class="s-open fa fa-search"></i><i class="s-close fa fa-times"></i></div><!-- toggle search button -->
-
-                    </div><!-- top search end -->
-
-                    <div class="k-breadcrumbs col-lg-12 clearfix"><!-- breadcrumbs -->
-
-                        <ol class="breadcrumb">
-                            <li><a href="index.html#">Home</a></li>
-                            <li class="active">Delhi College of Arts & Commerce</li>
-                        </ol>
-
-                    </div><!-- breadcrumbs end -->
-
-                </div><!-- row end -->
+                <br>
 
 
                 <!--******************************************LOG IN********************************************-->
--- sample-page.php
+++ sample-page.php
@@ -1,10 +1,337 @@
 <?php require('header.php');
 ?>
+<div class="col-md-12"><!-- container -->
 
+<h1>Theory</h1>
+
+<p>
+"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."</p>
+
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+
+ 
+<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.<p>
+
+<p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+
+<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
+
+
+<h1>Table 1</h1>
+<table class="table">
+<thead>
+                    <tr>
+                        <th>Rendering engine</th>
+                        <th>Browser</th>
+                        <th>Platform(s)</th>
+                        <th>Engine version</th>
+                        <th>CSS grade</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="odd gradeX">
+                        <td>Trident</td>
+                        <td>Internet Explorer 4.0</td>
+                        <td>Win 95+</td>
+                        <td class="center">4</td>
+                        <td class="center">X</td>
+                    </tr>
+                    <tr class="even gradeC">
+                        <td>Trident</td>
+                        <td>Internet Explorer 5.0</td>
+                        <td>Win 95+</td>
+                        <td class="center">5</td>
+                        <td class="center">C</td>
+                    </tr>
+                    <tr class="odd gradeA">
+                        <td>Trident</td>
+                        <td>Internet Explorer 5.5</td>
+                        <td>Win 95+</td>
+                        <td class="center">5.5</td>
+                        <td class="center">A</td>
+                    </tr>
+                    <tr class="even gradeA">
+                        <td>Trident</td>
+                        <td>Internet Explorer 6</td>
+                        <td>Win 98+</td>
+                        <td class="center">6</td>
+                        <td class="center">A</td>
+                    </tr>
+                    <tr class="odd gradeA">
+                        <td>Trident</td>
+                        <td>Internet Explorer 7</td>
+                        <td>Win XP SP2+</td>
+                        <td class="center">7</td>
+                        <td class="center">A</td>
+                    </tr>
+
+                </tbody>
+            </table>
+
+
+<h1>Table 2</h1>
+<table class="table table-striped">
+<thead>
+                    <tr>
+                        <th>Rendering engine</th>
+                        <th>Browser</th>
+                        <th>Platform(s)</th>
+                        <th>Engine version</th>
+                        <th>CSS grade</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="odd gradeX">
+                        <td>Trident</td>
+                        <td>Internet Explorer 4.0</td>
+                        <td>Win 95+</td>
+                        <td class="center">4</td>
+                        <td class="center">X</td>
+                    </tr>
+                    <tr class="even gradeC">
+                        <td>Trident</td>
+                        <td>Internet Explorer 5.0</td>
+                        <td>Win 95+</td>
+                        <td class="center">5</td>
+                        <td class="center">C</td>
+                    </tr>
+                    <tr class="odd gradeA">
+                        <td>Trident</td>
+                        <td>Internet Explorer 5.5</td>
+                        <td>Win 95+</td>
+                        <td class="center">5.5</td>
+                        <td class="center">A</td>
+                    </tr>
+                    <tr class="even gradeA">
+                        <td>Trident</td>
+                        <td>Internet Explorer 6</td>
+                        <td>Win 98+</td>
+                        <td class="center">6</td>
+                        <td class="center">A</td>
+                    </tr>
+                    <tr class="odd gradeA">
+                        <td>Trident</td>
+                        <td>Internet Explorer 7</td>
+                        <td>Win XP SP2+</td>
+                        <td class="center">7</td>
+                        <td class="center">A</td>
+                    </tr>
+
+                </tbody>
+            </table>
+
+
+<h1>Table 3</h1>
+<table class="table table-bordered">
+<thead>
+                    <tr>
+                        <th>Rendering engine</th>
+                        <th>Browser</th>
+                        <th>Platform(s)</th>
+                        <th>Engine version</th>
+                        <th>CSS grade</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="odd gradeX">
+                        <td>Trident</td>
+                        <td>Internet Explorer 4.0</td>
+                        <td>Win 95+</td>
+                        <td class="center">4</td>
+                        <td class="center">X</td>
+                    </tr>
+                    <tr class="even gradeC">
+                        <td>Trident</td>
+                        <td>Internet Explorer 5.0</td>
+                        <td>Win 95+</td>
+                        <td class="center">5</td>
+                        <td class="center">C</td>
+                    </tr>
+                    <tr class="odd gradeA">
+                        <td>Trident</td>
+                        <td>Internet Explorer 5.5</td>
+                        <td>Win 95+</td>
+                        <td class="center">5.5</td>
+                        <td class="center">A</td>
+                    </tr>
+                    <tr class="even gradeA">
+                        <td>Trident</td>
+                        <td>Internet Explorer 6</td>
+                        <td>Win 98+</td>
+                        <td class="center">6</td>
+                        <td class="center">A</td>
+                    </tr>
+                    <tr class="odd gradeA">
+                        <td>Trident</td>
+                        <td>Internet Explorer 7</td>
+                        <td>Win XP SP2+</td>
+                        <td class="center">7</td>
+                        <td class="center">A</td>
+                    </tr>
+
+                </tbody>
+            </table>
+
+<h1>Table 4</h1>
+<table class="table table-hover">
+<thead>
+                    <tr>
+                        <th>Rendering engine</th>
+                        <th>Browser</th>
+                        <th>Platform(s)</th>
+                        <th>Engine version</th>
+                        <th>CSS grade</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="odd gradeX">
+                        <td>Trident</td>
+                        <td>Internet Explorer 4.0</td>
+                        <td>Win 95+</td>
+                        <td class="center">4</td>
+                        <td class="center">X</td>
+                    </tr>
+                    <tr class="even gradeC">
+                        <td>Trident</td>
+                        <td>Internet Explorer 5.0</td>
+                        <td>Win 95+</td>
+                        <td class="center">5</td>
+                        <td class="center">C</td>
+                    </tr>
+                    <tr class="odd gradeA">
+                        <td>Trident</td>
+                        <td>Internet Explorer 5.5</td>
+                        <td>Win 95+</td>
+                        <td class="center">5.5</td>
+                        <td class="center">A</td>
+                    </tr>
+                    <tr class="even gradeA">
+                        <td>Trident</td>
+                        <td>Internet Explorer 6</td>
+                        <td>Win 98+</td>
+                        <td class="center">6</td>
+                        <td class="center">A</td>
+                    </tr>
+                    <tr class="odd gradeA">
+                        <td>Trident</td>
+                        <td>Internet Explorer 7</td>
+                        <td>Win XP SP2+</td>
+                        <td class="center">7</td>
+                        <td class="center">A</td>
+                    </tr>
+
+                </tbody>
+            </table>
+
+<h1>Table 5</h1>
+<table class="table table-condensed">
+<thead>
+                    <tr>
+                        <th>Rendering engine</th>
+                        <th>Browser</th>
+                        <th>Platform(s)</th>
+                        <th>Engine version</th>
+                        <th>CSS grade</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="odd gradeX">
+                        <td>Trident</td>
+                        <td>Internet Explorer 4.0</td>
+                        <td>Win 95+</td>
+                        <td class="center">4</td>
+                        <td class="center">X</td>
+                    </tr>
+                    <tr class="even gradeC">
+                        <td>Trident</td>
+                        <td>Internet Explorer 5.0</td>
+                        <td>Win 95+</td>
+                        <td class="center">5</td>
+                        <td class="center">C</td>
+                    </tr>
+                    <tr class="odd gradeA">
+                        <td>Trident</td>
+                        <td>Internet Explorer 5.5</td>
+                        <td>Win 95+</td>
+                        <td class="center">5.5</td>
+                        <td class="center">A</td>
+                    </tr>
+                    <tr class="even gradeA">
+                        <td>Trident</td>
+                        <td>Internet Explorer 6</td>
+                        <td>Win 98+</td>
+                        <td class="center">6</td>
+                        <td class="center">A</td>
+                    </tr>
+                    <tr class="odd gradeA">
+                        <td>Trident</td>
+                        <td>Internet Explorer 7</td>
+                        <td>Win XP SP2+</td>
+                        <td class="center">7</td>
+                        <td class="center">A</td>
+                    </tr>
+
+                </tbody>
+            </table>
+
+<h1>Table 6</h1>
+<table class="table ">
+<thead>
+                    <tr >
+                        <th>Rendering engine</th>
+                        <th>Browser</th>
+                        <th>Platform(s)</th>
+                        <th>Engine version</th>
+                        <th>CSS grade</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="success">
+                        <td>Trident</td>
+                        <td>Internet Explorer 4.0</td>
+                        <td>Win 95+</td>
+                        <td class="center">4</td>
+                        <td class="center">X</td>
+                    </tr>
+                    <tr class="warning">
+                        <td>Trident</td>
+                        <td>Internet Explorer 5.0</td>
+                        <td>Win 95+</td>
+                        <td class="center">5</td>
+                        <td class="center">C</td>
+                    </tr>
+                    <tr class="danger">
+                        <td>Trident</td>
+                        <td>Internet Explorer 5.5</td>
+                        <td>Win 95+</td>
+                        <td class="center">5.5</td>
+                        <td class="center">A</td>
+                    </tr>
+                    <tr class="info">
+                        <td>Trident</td>
+                        <td>Internet Explorer 6</td>
+                        <td>Win 98+</td>
+                        <td class="center">6</td>
+                        <td class="center">A</td>
+                    </tr>
+                    <tr class="active">
+                        <td>Trident</td>
+                        <td>Internet Explorer 7</td>
+                        <td>Win XP SP2+</td>
+                        <td class="center">7</td>
+                        <td class="center">A</td>
+                    </tr>
+
+                </tbody>
+            </table>
+</div>
 
 </div><!-- container end -->
 
-        </div><!-- content wrapper end -->
+</div><!-- content wrapper end -->
 
 <?php require('footer.php');
 
