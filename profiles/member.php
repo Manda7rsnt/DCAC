3611922b7f32d8f1fe1b3109124cd8da07764201
ashish-shrma <ashish851995@gmail.com>
2015-08-14 03:30:35 +0530

    admin account

--- profiles/member.php
+++ profiles/member.php
@@ -41,23 +41,70 @@ if(!isset($_SESSION["sess_user"])){
 
 while ($row = mysqli_fetch_array($result))  
 {
+    
+    
     $department= $row["user_papers"];
     
+    
         if($department!=$department2){
-        echo"<h4>"."Department of ".$department."</h4>";
+        
+            
+             if($num==0)
+        {
+        echo"<h2>"."Department of ".$department."</h2>";
+            $count=0;
+        
+        echo '<table class="table">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Faculty Name</th>
+                        <th>Designation</th>
+                    </tr>
+                </thead>
+                <tbody>';
+        $num=1;
+        
+    }
+            else
+        {    
+            echo ' </tbody>
+            </table>';
+            
+            echo"<h2>"."Department of ".$department."</h2>";
             $count=0;
             
+            echo '<table class="table">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Faculty Name</th>
+                        <th>Designation</th>
+                    </tr>
+                </thead>
+                <tbody>';
+            }
+            
+            
+            
     }
+    
     if($row["user_lastname"]=='lastname'){
         $row["user_lastname"]='';
     }
+    
     $id=$row["user_id"];
     $count=$count+1;
    
-    echo $count.". ".$row["user_prefix"]." ".$row["user_firstname"]." ".$row["user_lastname"]."   ---   ".$row["user_password"]."<br>" ;
+    echo '<tr>';
+    echo "<td>".$count."</td>";
+    echo '<td>'.$row["user_prefix"].' '.$row["user_firstname"].' '.$row["user_lastname"].'</td>';
+    
+    echo '<td>'.$row["user_password"].'</td>' ;
+    echo '</tr>';
     $department2=$row["user_papers"];
     
-} ?>
+}    ?>
 </div>
 </body>
 </html>
