b3a4e8407665c6dba621ac749f90f8cb4f26303e
Manda7rsnt <madridist14@gmail.com>
2015-08-16 15:54:45 +0530

    DCAC
    Internal Assessment

---
+++ internalassessment.php
@@ -0,0 +1,43 @@
+<?php require('header.php');
+?>
+<div class="col-md-12"><!-- container -->
+
+
+
+<p><h2>General Office</h2></p>
+          
+            <table class ="table">
+               <thead>
+                <tr>
+                 <th>S.No.</th>
+                 <th>Second Semester</th>		
+                 <th>Fourth Semester</th>
+                 <th>Sixth Semester</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr class="success"><td>1.</td><td><a href="">	Economics (Hons.)</a> </td><td> <a href="">Economics (Hons.)</a></td> <td><a href="">Economics (Hons.)</a></td></tr>
+                <tr class="warning"><td>2.</td><td>	<a href="">English (Hons.) </a></td><td><a href="">English (Hons.)</a></td><td><a href="">English (Hons.)</a></td></tr>
+                <tr class="danger"><td>3.</td><td>	<a href="">History (Hons.) </a></td><td><a href="">History (Hons.)</a></td><td><a href="">History (Hons.)</a></td></tr>
+                <tr class="info"><td>4.</td><td> <a href="">Political Science (Hons.) </a></td><td><a href="">Political Science (Hons.) </a></td><td><a href="">Political Science (Hons.)</a></td></tr>
+                <tr class="active"><td>5.</td><td>	<a href="">Journalism (Hons.) </a></td><td> <a href="">Journalism (Hons.)</a></td><td><a href="">Journalism (Hons.)</a></td></tr>
+                <tr class="success"><td>6.</td><td>	<a href="">BCOM (Hons) </a></td><td> <a href="">BCOM (Hons)</a></td><td><a href="">BCOM (Hons)</a></td></tr>
+                <tr class="warning"><td>7.</td><td>	<a href="">BCOM (Prog) </a></td><td> -- </td><td><a href="">BCOM (Prog)</a></td></tr>
+                <tr class="danger"><td>8.</td><td>	<a href="">BA (Prog) </a></td><td>	-- </td><td><a href="">BA (Prog)</a></td></tr>
+                </tbody>
+                </table>
+
+
+
+
+
+
+</div>
+
+</div><!-- container end -->
+
+</div><!-- content wrapper end -->
+
+<?php require('footer.php');
+
+?>
